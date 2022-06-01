e20652833005de290bcf9ae2f20394d105eb64d4
Himenon <k.himeno314@gmail.com>
2022-06-01 19:31:37 +0900

    chore: update

--- packages/esm/src/index.mjs
+++ packages/esm/src/index.mjs
@@ -1,5 +1,5 @@
 import mainPkg from "@himenon/template-js";
-import subPkg from "@himenon/template-js/$esm/sub.js";
+import subPkg from "@himenon/template-js/sub.js";
 
 mainPkg.Hello.thisIsSubDirectoryFile();
 mainPkg.Sub.thisIsSub();
